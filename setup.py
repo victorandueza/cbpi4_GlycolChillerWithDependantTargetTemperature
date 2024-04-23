417173cf610e626253d4aac378e501084e733b4b
Víctor Andueza <vandueza@bcn3d.com>
2024-04-23 11:14:01 +0200

    Update version to 0.0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='cbpi4_GlycolChillerWithDependantTargetTemperature',
-      version='0.0.1',
+      version='0.0.2',
       description='CraftBeerPi Plugin',
       author='Víctor Andueza',
       author_email='vandueza13@gmail.com',
